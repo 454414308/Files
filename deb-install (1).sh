9fd49dc6ebcc657d771e9f986a36f98d941e6be8
454414308 <454414308@qq.com>
2024-06-20 20:53:24 +0800

    Add files via upload

---
+++ deb-install (1).sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+ARCH_RAW=$(uname -m)
+case "${ARCH_RAW}" in
+    'x86_64')    ARCH='amd64';;
+    'x86' | 'i686' | 'i386')     ARCH='386';;
+    'aarch64' | 'arm64') ARCH='arm64';;
+    'armv7l')   ARCH='armv7';;
+    's390x')    ARCH='s390x';;
+    *)          echo "Unsupported architecture: ${ARCH_RAW}"; exit 1;;
+esac
+
+VERSION=1.9.0
+
+curl -Lo sing-box.deb "https://github.com/SagerNet/sing-box/releases/download/v${VERSION}/sing-box_${VERSION}_linux_${ARCH}.deb"
+sudo dpkg -i sing-box.deb
+rm sing-box.deb
+
