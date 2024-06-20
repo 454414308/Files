af95a539db8d8dc65e04a086a4fe9c3c46e6d16d
454414308 <454414308@qq.com>
2024-06-20 21:04:01 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-
-set -e -o pipefail
-
-ARCH_RAW=$(uname -m)
-case "${ARCH_RAW}" in
-    'x86_64')    ARCH='amd64';;
-    'x86' | 'i686' | 'i386')     ARCH='386';;
-    'aarch64' | 'arm64') ARCH='arm64';;
-    'armv7l')   ARCH='armv7';;
-    's390x')    ARCH='s390x';;
-    *)          echo "Unsupported architecture: ${ARCH_RAW}"; exit 1;;
-esac
-
 curl -Lo sing-box.deb "https://github.com/454414308/Files/blob/main/sing-box_1.9.0_linux_amd64.deb"
 sudo dpkg -i sing-box.deb
 rm sing-box.deb
